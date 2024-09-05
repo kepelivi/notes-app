8c6d0f58c3ece7408909f739a43ac104db8a4730
kepelivi <kepelivi7@gmail.com>
2024-09-05 17:50:46 +0200

    create - Note class

--- app/src/main/java/Note.kt
+++ app/src/main/java/Note.kt
@@ -1,6 +1,8 @@
+import java.util.UUID
+
 class Note (
     var id: String = "",
     var title: String = "",
     var content: String = "",
-    var timestamp: Long = 0L
+    var timestamp: String = ""
 )
